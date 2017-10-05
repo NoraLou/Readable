b4f9e0fda19537f9952d8a41d0ef9ef9fed2af6e
NoraLou <nora.humpage@gmail.com>
2017-10-04 20:14:38 -0700

    clean up a bit

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,18 +1,6 @@
 const URL = 'http://localhost:3001/'
 const auth = 'super_secret'
 
-// export const getAllPosts = () => {
-//    const url = 'http://localhost:3001/posts';
-//    fetch(url, { headers: { 'Authorization': 'whatever-you-want' }} )
-//      .then( (res) => { return(res.text()) })
-//      .then((data) => {
-//        console.log("data :", JSON.parse(data))
-//        var p = []
-//        p = JSON.parse(data)
-//        console.log("p :", p)
-//      });
-//  }
-
 export const fetchAllPosts = () => {
   return fetch( `${URL}posts`, { headers: { 'Authorization': auth }})
     .then(res => res.text())
@@ -22,18 +10,6 @@ export const fetchAllPosts = () => {
     })
 }
 
-// export const fetchAllPosts = () => {
-//   return fetch( `${URL}posts`, { headers: { 'Authorization': auth }})
-//     .then((res) => res.json())
-//     .then( ({posts}) => posts.map( ({post})  => post)        )
-//     .catch((error) => {
-//       console.log(`Error : ${error}`)
-//     })
-// }
-
-// export function createPost() {
-//   r
-// }
 
 
 
