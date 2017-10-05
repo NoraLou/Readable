3dc4495f7ae0d20d820d63bdf7c1f3885cd63411
NoraLou <nora.humpage@gmail.com>
2017-10-05 15:16:19 -0700

    sort reflects state change, remove test data

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -10,7 +10,6 @@ class Home extends Component {
     constructor(props) {
       super(props)
       this.state = {
-
         sortBy : this.voteSort
       }
     }
@@ -27,63 +26,11 @@ class Home extends Component {
       }
     }
 
-    test = [
-    {id: '6ni6ok3ym7mf1p33lnez',
-    timestamp: 1468479767190,
-    title: 'Learn Redux in 10 minutes!',
-    body: 'Just kidding. It takes more than 10 minutes to learn technology.',
-    author: 'thingone',
-    category: 'redux',
-    voteScore: -5,
-    deleted: false},
-
-    {id: '8xf0y6ziyjabvozdd253nd',
-    timestamp: 1467166872634,
-    title: 'Udacity is the best place to learn React',
-    body: 'Everyone says so after all.',
-    author: 'thingtwo',
-    category: 'react',
-    voteScore: 6,
-    deleted: false},
-
-    {id: '8jsdjhfd',
-    timestamp: 1467166872634,
-    title: 'Udacity is the best place to learn React',
-    body: 'Everyone says so after all.',
-    author: 'thingtwo',
-    category: 'react',
-    voteScore: 3,
-    deleted: false},
-
-    {id: 'dd253nd',
-    timestamp: 1507235633036,
-    title: 'Udacity is the best place to learn React',
-    body: 'Everyone says so after all.',
-    author: 'thingtwo',
-    category: 'react',
-    voteScore: 1,
-    deleted: false},
-
-    {id: 'zdd253nd',
-    timestamp: 1507235733680,
-    title: 'Udacity is the best place to learn React',
-    body: 'Everyone says so after all.',
-    author: 'thingtwo',
-    category: 'react',
-    voteScore: -22,
-    deleted: false},
-  ]
-
-
-  sortByOptions = ['Vote Score', 'Date']
-
-// <ListGroupItem header={title} onClick={(evt) => props.navToPost(evt, post.id, categoryName)}>
-
   render() {
 
     const { posts } = this.props
 
-    const postsReadableDate = this.test.map( post  => {
+    const postsReadableDate = posts.map( post  => {
       let date = new Date (post.timestamp)
       let formatDate = date.toDateString()
       return {
@@ -102,12 +49,11 @@ class Home extends Component {
 
         <div className='container'>
           <div>SORT BY :</div>
-          <a href="#voteScore" onClick={() => this.setState(this.state.sortBy: 'voteScore')}> Vote Score </a>
-          <a href="#date" onClick={() => this.setState(this.state.sortBy: 'voteScore')}> Date </a>
+          <a href="#score" onClick={() => this.setState( {sortBy: this.voteSort} )}> Score </a>
+          <a href="#date" onClick={() => this.setState( {sortBy: this.dateSort} )}> Date </a>
         </div>
-
         <Grid className="post-list">
-          {postsReadableDate.sort(this.dateSort).map((post) => (
+          {postsReadableDate.sort(this.state.sortBy).map((post) => (
             <Row className="post-card" key={post.id}>
               <Col xs={8} className="post-grid-item">
                 <div className="content">
@@ -133,7 +79,6 @@ class Home extends Component {
               </Col>
             </Row>
           ))}
-
         </Grid>
 
       </div>
